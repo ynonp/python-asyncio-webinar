0379249558dd366f246d34eb5a39eb9c312080ea
ynonp <ynonperek@gmail.com>
2019-11-28 10:41:05 +0200

    FIXED aiofiles demo

--- create_files.py
+++ create_files.py
@@ -2,15 +2,18 @@ import asyncio, aiofiles, logging
 
 async def create_file(filename):
     async with aiofiles.open(filename, mode='w') as f:
-        for _ in range(10_000):
-            f.write('Hello\n')
+        for _ in range(1_000):
+            await f.write('Hello\n')
 
         print(f"Created file {filename}")
 
 async def main():
-    await create_file('one.txt')
-    await create_file('two.txt')
-    await create_file('three.txt')
+    t1 = asyncio.create_task(create_file('one.txt'))
+    t2 = asyncio.create_task(create_file('two.txt'))
+    t3 = asyncio.create_task(create_file('three.txt'))
+
+    for t in [t1, t2, t3]:
+        await t
 
 logging.basicConfig(level=logging.DEBUG)
 asyncio.run(main(), debug=True)
